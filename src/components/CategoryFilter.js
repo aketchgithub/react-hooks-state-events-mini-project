7498c3919b1fcbaafb99b3d29f8c7655d0fc44cf
@aketchgithub <vickeryace@gmaill.com>
2023-01-18 14:44:29 +0300

    Complete

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,18 @@
 import React from "react";
-
 function CategoryFilter() {
+function CategoryFilter({categories,click}) {
+  const buttons = categories.map((category, index) => {
+    return (
+      <button key={index} className={category} onClick={() => click(category)} >{category} </button>
+    )
+  });
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {buttons}
     </div>
   );
 }
 
-export default CategoryFilter;
+}
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,8 +1,19 @@
+
 import React from "react";
 
-function NewTaskForm() {
+function NewTaskForm({onTaskFormSubmit, categories}) {
+  let newArr = categories.filter(cat => cat !== "All").map((cat, ind) => <option key={ind}>{cat}</option>);
   return (
     <form className="new-task-form">
+    <form className="new-task-form"
+    onSubmit={function (event) {
+      event.preventDefault();
+      let newTask = document.getElementsByName("text")[0].value;
+      let taskCategory = document.getElementsByName("category")[0].value;
+      onTaskFormSubmit({ "text": newTask, "category": taskCategory })
+    }}
+    autoComplete="off"
+    >
       <label>
         Details
         <input type="text" name="text" />
@@ -11,11 +22,10 @@ function NewTaskForm() {
         Category
         <select name="category">
           {/* render <option> elements for each category here */}
+          {newArr}
         </select>
       </label>
       <input type="submit" value="Add task" />
-    </form>
-  );
-}
 
-export default NewTaskForm;
+)}
+
